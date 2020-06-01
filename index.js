e7302578487f3b094f98ffe2fe15de9a52bf4c4f
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-01 13:44:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
+let username = '';
 
+function pull(username){
+    fetch(`https://api.github.com/users/${username}/repos`)
+    // need to add in the .then logic including an invalid request
+        .then(response => response.json())
+        .then (responseJson => {
+            if (true){ // this indicator for success is not working
+                responseHandler(responseJson.message);
+                console.log("success");
+            }
+            else {
+                console.log("error")
+                //throw new Error (responseJson);
+            }
+        });
+}
+
+function responseHandler(response){
+    //this function will format the API response
+    console.log(response);
+}
+
+function display(){
+    //this function will display the processed response
+}
 
 function begin(){
     $('.submit').on("click", function(e){
-        console.log("submit pressed")
+        e.preventDefault();
+        username = $('#handle').val();
+        pull(username);
     });
 }
 
