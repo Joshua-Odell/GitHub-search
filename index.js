72f468e0f3b464ee67ce34cfa2dfc3e333900cca
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-02 14:21:10 -0500

    cleaned up the code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ function pull(username){
     // need to add in the .then logic including an invalid request
         .then(response => response.json())
         .then (responseJson => {
-            //console.log(responseJson);
             if (true){ // this indicator for success is not working
                 responseHandler(responseJson);
                 console.log("success");
@@ -23,16 +22,11 @@ function responseHandler(response){
     //this should for loop through the returned data selecting the repo name and the link to the repo then appending to a list.
     //let result = response.map
     let result = ""
-    console.log(response.length)
     for (i=0; i<response.length; i++){
         name = response[i].name;
-        console.log(name);
         link = response[i].html_url;
-        console.log(link);
         temp = `<li> <a href=${link}>${name}</a>`;
-        console.log(temp);
         result = result + temp;
-        console.log(result);
     }
     display(result);
 }
