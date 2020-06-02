a7fc117e0176c65aadf406efd728ef10285f0c5a
Josh Odell <52717569+Joshua-Odell@users.noreply.github.com>
2020-06-02 14:17:46 -0500

    Finished

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-let username = '';
+
 
 function pull(username){
     fetch(`https://api.github.com/users/${username}/repos`)
     // need to add in the .then logic including an invalid request
         .then(response => response.json())
         .then (responseJson => {
+            //console.log(responseJson);
             if (true){ // this indicator for success is not working
-                responseHandler(responseJson.message);
+                responseHandler(responseJson);
                 console.log("success");
+                
             }
             else {
                 console.log("error")
@@ -18,17 +20,35 @@ function pull(username){
 
 function responseHandler(response){
     //this function will format the API response
-    console.log(response);
+    //this should for loop through the returned data selecting the repo name and the link to the repo then appending to a list.
+    //let result = response.map
+    let result = ""
+    console.log(response.length)
+    for (i=0; i<response.length; i++){
+        name = response[i].name;
+        console.log(name);
+        link = response[i].html_url;
+        console.log(link);
+        temp = `<li> <a href=${link}>${name}</a>`;
+        console.log(temp);
+        result = result + temp;
+        console.log(result);
+    }
+    display(result);
 }
 
-function display(){
+
+function display(result){
     //this function will display the processed response
+    $('.response-list').empty();
+    $('.response-list').html(result);
+    // remove hidden class
 }
 
 function begin(){
     $('.submit').on("click", function(e){
         e.preventDefault();
-        username = $('#handle').val();
+        let username = $('#handle').val();
         pull(username);
     });
 }
